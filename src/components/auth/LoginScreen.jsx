c7261023e3a1d03fe9641346ae1261385c988636
stargazing4122 <jadenyeiden@gmail.com>
2022-03-01 21:15:00 -0500

    interfaz login

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const LoginScreen = () => {
   return (
-    <div>
-      Login Screen
+    <div className="login-container">
+      <p className="login__icon">
+        <i className="fa-solid fa-user"></i>Login
+      </p>
+      <div className="login__button">
+        <div className="google-btn">
+          <div className="google-icon-wrapper">
+            <img
+              className="google-icon"
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              alt="google button"
+            />
+          </div>
+          <p className="btn-text">
+            <b>Sign in with google</b>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
