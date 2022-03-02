f57c3f8a176d17052c2efb6a7e1765d22f1b29c0
stargazing4122 <jadenyeiden@gmail.com>
2022-03-01 23:15:56 -0500

    public and private routes added

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import LoginScreen from '../components/auth/LoginScreen'
-import JournalScreen from '../components/journal/JournalScreen'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { auth } from '../firebase/config';
+
+import LoginScreen from '../components/auth/LoginScreen';
+import JournalScreen from '../components/journal/JournalScreen';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+import ChekingScreen from '../components/checking/ChekingScreen';
 
 const AppRouter = () => {
+  //hooks
+  const [isAuth, setIsAuth] = useState(false);
+  const [isChecking, setIsChecking] = useState(true);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user?.uid) {
+        setIsAuth(true);
+      } else {
+        setIsAuth(false);
+      }
+      setIsChecking(false);
+    });
+  }, []);
+
+  //other
+  if (isChecking) {
+    return <ChekingScreen />;
+  }
+
   return (
     <Router>
       <Switch>
-        <Route 
+        <PublicRoute
           exact
-          path="/auth" 
-          component={LoginScreen} 
+          path="/auth"
+          component={LoginScreen}
+          isAuth={isAuth}
         />
-        <Route 
+        <PrivateRoute
           exact
-          path="/" 
-          component={JournalScreen} 
+          path="/"
+          component={JournalScreen}
+          isAuth={isAuth}
         />
-
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
 export default AppRouter;
